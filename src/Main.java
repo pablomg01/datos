bad653f581ec76c29b7b098ca18efdc0bb1c53d6
pablo <pablomg01@hotmail.com>
2023-07-11 21:22:14 +0200

    pruebaGit5

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hola Mundo como vas estas cambiando");
+        System.out.println("Hola Mundo como vas estas cambiando gjdsjgl mglsg");
     }
 }
